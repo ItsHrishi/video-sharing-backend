b415e5e11a531e6c643ddf4100ab8c97936af493
Hrishikesh <ghrishikesh77@gmail.com>
2024-12-16 13:25:48 +0530

    connect to mongo atlas

---
+++ src/db/index.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+const connectDb = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGO_DB_URI}/${DB_NAME}`
+    );
+    // check:  what is this connection instance
+    console.log(
+      `MongoDb connected!! Connection host : ${connectionInstance.connection.host}`
+    );
+    // console.log(connectionInstance);
+  } catch (error) {
+    console.log("MONGODB connection FAILED!! : ", error);
+    // check:  what are the exit code and what is process.exit?
+    process.exit(1);
+  }
+};
+
+export default connectDb;
---
+++ src/index.js
@@ -0,0 +1,31 @@
+// require("dotenv").config({ path: "./env" }); // old way to do it
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb();
+
+/*
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("Error : ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT, () => {
+      console.log(`App listening on port : ${process.env.PORT}`);
+    });
+  } catch (error) {
+    console.error(error);
+    throw err;
+  }
+})();
+
+*/
