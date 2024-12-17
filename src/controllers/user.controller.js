e99b5243652b1e2216b5b9693bccfb0beb4d045f
Hrishikesh <ghrishikesh77@gmail.com>
2024-12-17 23:01:18 +0530

    add user chanel data feature -> aggregation pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -345,6 +345,79 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     );
 });
 
+//we are getting any channels profile here
+const getUserChanelProfile = asyncHandler(async (req, res) => {
+  const { userName } = req.params;
+
+  if (!userName) {
+    throw new ApiError(400, "Username is missing");
+  }
+
+  // aggregte is a method which takes multiple pipelines
+
+  const channel = await User.aggregate([
+    {
+      $match: {
+        userName: userName?.toLowerCase(),
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "channel",
+        as: "subscribers",
+      },
+    },
+    {
+      $lookup: {
+        from: "subscriptions",
+        localField: "_id",
+        foreignField: "subscriber",
+        as: "subscribedTo",
+      },
+    },
+    {
+      $addFields: {
+        subscribersCount: {
+          $size: "$subscribers",
+        },
+        channelsSubscribedToCount: {
+          $size: "$subscribedTo",
+        },
+        // the in check in objects and arrays
+        isSubscribed: {
+          $cond: {
+            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+            then: true,
+            else: false,
+          },
+        },
+      },
+    },
+    {
+      $project: {
+        fullName: 1,
+        userName: 1,
+        subscribersCount: 1,
+        channelsSubscribedToCount: 1,
+        isSubscribed: 1,
+        avatar: 1,
+        coverImage: 1,
+        email: 1,
+      },
+    },
+  ]);
+
+  if (!channel?.length) {
+    throw new ApiError(404, "Channel does not exist");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, "User channel frtched successfullys"));
+});
+
 export {
   registerUser,
   loginUser,
@@ -355,4 +428,5 @@ export {
   updateAccountdetais,
   updateUserAvatar,
   updateUserCoverImage,
+  getUserChanelProfile,
 };
